05d70a581e7a72d90627cd587b3d7111b8f5436f
jgelia <jgelia@ncsu.edu>
2024-08-31 15:36:13 -0400

    Clean up

--- Scripts/test_servo.py
+++ Scripts/test_servo.py
@@ -1,16 +1,12 @@
 """
-Run as `python -m Scripts.test_airbrakes`
+Make sure you are in the root directory of the project, not inside Scripts, and run the following command:
+`python -m Scripts.test_servo`
+For the pi, you will have to use python3
 """
 from Airbrakes.servo import Servo
 
-# this is the pin that the servo's data wire is plugged into
-SERVO_PIN = 32
-
-SERVO_CLOSED_DUTY = 6.3
-SERVO_OPEN_DUTY = 9.2
-servo = Servo(SERVO_PIN, SERVO_OPEN_DUTY, SERVO_CLOSED_DUTY)
+servo = Servo()
 
 print("Type (1) to deploy and (0) to retract the airbrakes.")
 while True:
-    command = float(input())
-    servo.set_extension(command)
+    servo.set_extension(float(input()))
