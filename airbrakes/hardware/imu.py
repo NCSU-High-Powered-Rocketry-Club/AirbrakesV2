2b5ea30035d191820c868ae804b4ff7aadb2074c
jgelia <jgelia@ncsu.edu>
2024-09-20 16:40:43 -0400

    Created AbstractIMU

--- airbrakes/hardware/imu.py
+++ airbrakes/hardware/imu.py
@@ -3,6 +3,7 @@
 import collections
 import multiprocessing
 import warnings
+from abc import ABC, abstractmethod
 
 try:
     import mscl
@@ -17,7 +18,59 @@ from airbrakes.data_handling.imu_data_packet import EstimatedDataPacket, IMUData
 from constants import ESTIMATED_DESCRIPTOR_SET, MAX_QUEUE_SIZE, RAW_DESCRIPTOR_SET
 
 
-class IMU:
+class AbstractIMU(ABC):
+    """
+    Abstract base class for interacting with the IMU (Inertial Measurement Unit) on the rocket.
+    Any IMU implementation should inherit from this class and implement the necessary methods to start, stop, and
+    retrieve data from the IMU.
+    """
+
+    __slots__ = (
+        "_data_fetch_process",
+        "_data_queue",
+        "_running",
+    )
+
+    @property
+    @abstractmethod
+    def is_running(self) -> bool:
+        """
+        Returns whether the process fetching data from the IMU is running.
+        """
+
+    @abstractmethod
+    def start(self) -> None:
+        """
+        Starts the process fetching data from the IMU.
+        """
+
+    @abstractmethod
+    def stop(self) -> None:
+        """
+        Stops the process fetching data from the IMU. This should be called when the program is shutting down.
+        """
+
+    @abstractmethod
+    def get_imu_data_packet(self) -> IMUDataPacket | None:
+        """
+        Gets the last available data packet from the IMU.
+
+        Note: If `get_imu_data_packet` is called slower than the frequency set, the data will not
+        be the latest, but the first in the queue.
+
+        :return: an IMUDataPacket object containing the latest data from the IMU. If a value is
+            not available, it will be None.
+        """
+
+    @abstractmethod
+    def get_imu_data_packets(self) -> collections.deque[IMUDataPacket]:
+        """Returns all available data packets from the IMU.
+
+        :return: A deque containing the specified number of data packets
+        """
+
+
+class IMU(AbstractIMU):
     """
     Represents the IMU on the rocket. It's used to get the current acceleration of the rocket. This is used to interact
     with the data collected by the Parker-LORD 3DMCX5-AR.
@@ -27,12 +80,6 @@ class IMU:
 
     Here is the setup docs: https://github.com/LORD-MicroStrain/MSCL/blob/master/HowToUseMSCL.md
     """
-
-    __slots__ = (
-        "_data_fetch_process",
-        "_data_queue",
-        "_running",
-    )
 
     def __init__(self, port: str, frequency: int, upside_down: bool):
         # Shared Queue which contains the latest data from the IMU. The MAX_QUEUE_SIZE is there
@@ -48,17 +95,33 @@ class IMU:
 
     @property
     def is_running(self) -> bool:
-        """
-        Returns whether the process fetching data from the IMU is running.
-        """
         return self._running.value
 
     def start(self) -> None:
-        """
-        Starts the process fetching data from the IMU.
-        """
         self._running.value = True
         self._data_fetch_process.start()
+
+    def stop(self) -> None:
+        self._running.value = False
+        # Waits for the process to finish before stopping it
+        self._data_fetch_process.join()
+
+    def get_imu_data_packet(self) -> IMUDataPacket | None:
+        return self._data_queue.get()
+
+    def get_imu_data_packets(self) -> collections.deque[IMUDataPacket]:
+        """Returns all available data packets from the IMU.
+
+        :return: A deque containing the specified number of data packets
+        """
+
+        data_packets = collections.deque()
+
+        while not self._data_queue.empty():
+            data_packet = self.get_imu_data_packet()
+            data_packets.append(data_packet)
+
+        return data_packets
 
     def _fetch_data_loop(self, port: str, frequency: int, _: bool) -> None:
         """
@@ -122,37 +185,3 @@ class IMU:
 
                 # Put the latest data into the shared queue
                 self._data_queue.put(imu_data_packet)
-
-    def get_imu_data_packet(self) -> IMUDataPacket | None:
-        """
-        Gets the last available data packet from the IMU.
-
-        Note: If `get_imu_data_packet` is called slower than the frequency set, the data will not
-        be the latest, but the first in the queue.
-
-        :return: an IMUDataPacket object containing the latest data from the IMU. If a value is
-            not available, it will be None.
-        """
-        return self._data_queue.get()
-
-    def get_imu_data_packets(self) -> collections.deque[IMUDataPacket]:
-        """Returns all available data packets from the IMU.
-
-        :return: A deque containing the specified number of data packets
-        """
-
-        data_packets = collections.deque()
-
-        while not self._data_queue.empty():
-            data_packet = self.get_imu_data_packet()
-            data_packets.append(data_packet)
-
-        return data_packets
-
-    def stop(self) -> None:
-        """
-        Stops the process fetching data from the IMU. This should be called when the program is shutting down.
-        """
-        self._running.value = False
-        # Waits for the process to finish before stopping it
-        self._data_fetch_process.join()
