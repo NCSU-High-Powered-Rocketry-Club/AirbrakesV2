c87571419eaf30ca2273a572eda00f016a4b2f64
jgelia <jgelia@ncsu.edu>
2024-09-27 14:20:05 -0400

    stuff

--- airbrakes/hardware/servo.py
+++ airbrakes/hardware/servo.py
@@ -1,4 +1,6 @@
 """Module which contains the Servo class, representing a servo motor that controls the extension of the airbrakes."""
+import threading
+import time
 
 import gpiozero
 
@@ -24,7 +26,25 @@ class Servo:
 
         self.servo = gpiozero.Servo(gpio_pin_number)
 
-    def set_extension(self, extension: ServoExtension):
+    def set_extended(self):
+        thread = threading.Thread(target=self._extend_then_no_buzz)
+        thread.start()
+
+    def set_retracted(self):
+        thread = threading.Thread(target=self._retract_then_no_buzz)
+        thread.start()
+
+    def _extend_then_no_buzz(self):
+        self._set_extension(ServoExtension.MAX_EXTENSION)
+        time.sleep(1)
+        self._set_extension(ServoExtension.MAX_NO_BUZZ)
+
+    def _retract_then_no_buzz(self):
+        self._set_extension(ServoExtension.MIN_EXTENSION)
+        time.sleep(1)
+        self._set_extension(ServoExtension.MIN_NO_BUZZ)
+
+    def _set_extension(self, extension: ServoExtension):
         """
         Sets the extension of the servo.
         :param extension: The extension of the servo, there are 4 possible values, see constants.
