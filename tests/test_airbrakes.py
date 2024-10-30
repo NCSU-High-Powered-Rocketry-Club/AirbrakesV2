77e382dda5ccd9125d34d5cc269cf510d41dc020
Harshil <37377066+harshil21@users.noreply.github.com>
2024-10-29 21:55:21 -0400

    Fix airbrakes.update tests

--- tests/test_airbrakes.py
+++ tests/test_airbrakes.py
@@ -4,7 +4,7 @@ import pytest
 
 from airbrakes.airbrakes import AirbrakesContext
 from airbrakes.data_handling.data_processor import IMUDataProcessor
-from airbrakes.state import StandByState
+from airbrakes.state import CoastState, StandByState
 from constants import SERVO_DELAY, ServoExtension
 
 
@@ -93,44 +93,53 @@ class TestAirbrakesContext:
         calls = []
         asserts = []
 
-        def update(self, est_data_packets):
+        def data_processor_update(self, est_data_packets):
             # monkeypatched method of IMUDataProcessor
             calls.append("update called")
             # Length of these lists must be equal to the number of estimated data packets for
             # get_processed_data() to work correctly
             self._current_altitudes = [0.0] * len(est_data_packets)
             self._vertical_velocities = [0.0] * len(est_data_packets)
+            self._rotated_accelerations = [0.0] * len(est_data_packets)
+            self._time_differences = [0.0] * len(est_data_packets)
 
         def state(self):
             # monkeypatched method of State
             calls.append("state update called")
 
-        def log(self, state, extension, imu_data_packets, processed_data_packets):
+        def log(self, state, extension, imu_data_packets, processed_data_packets, apogee):
             # monkeypatched method of Logger
             calls.append("log called")
             asserts.append(len(imu_data_packets) > 10)
-            asserts.append(state == "S")
+            asserts.append(state == "C")
             asserts.append(extension == ServoExtension.MIN_EXTENSION.value)
             asserts.append(imu_data_packets[0].timestamp == pytest.approx(time.time(), rel=1e9))
+            # asserts.append()
+
+        def apogee_update(self, processed_data_packets):
+            calls.append("apogee update called")
+
 
         mocked_airbrakes = AirbrakesContext(servo, random_data_mock_imu, logger, data_processor, apogee_predictor)
+        mocked_airbrakes.state = CoastState(mocked_airbrakes)  # Set to coast state to test apogee update
         mocked_airbrakes.start()
 
         time.sleep(0.01)  # Sleep a bit so that the IMU queue is being filled
 
         assert mocked_airbrakes.imu._data_queue.qsize() > 0
-        assert mocked_airbrakes.state.name == "StandByState"
+        assert mocked_airbrakes.state.name == "CoastState"
         assert mocked_airbrakes.data_processor._last_data_packet is None
 
-        monkeypatch.setattr(data_processor.__class__, "update", update)
+        monkeypatch.setattr(data_processor.__class__, "update", data_processor_update)
+        monkeypatch.setattr(apogee_predictor.__class__, "update", apogee_update)
         monkeypatch.setattr(mocked_airbrakes.state.__class__, "update", state)
         monkeypatch.setattr(logger.__class__, "log", log)
 
         # Let's call .update() and check if stuff was called and/or changed:
         mocked_airbrakes.update()
 
-        assert len(calls) == 3
-        assert calls == ["update called", "state update called", "log called"]
+        assert len(calls) == 4
+        assert calls == ["update called", "apogee update called", "state update called", "log called"]
         assert all(asserts)
 
         mocked_airbrakes.stop()
