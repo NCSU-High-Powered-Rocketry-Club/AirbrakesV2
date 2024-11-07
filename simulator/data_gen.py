49760990b29ed3cdd6b1f01b4529488373c463a9
wlsanderson <wlsanderson5@gmail.com>
2024-11-06 21:50:29 -0500

    reformatting

--- simulator/data_gen.py
+++ simulator/data_gen.py
@@ -10,6 +10,7 @@ from scipy.spatial.transform import Rotation as R
 
 from airbrakes.data_handling.imu_data_packet import (
     EstimatedDataPacket,
+    IMUDataPacket,
     RawDataPacket,
 )
 from constants import ACCELERATION_NOISE_THRESHOLD, GRAVITY
@@ -24,7 +25,6 @@ class DataGenerator:
 
     __slots__ = (
         "_config",
-        "_current_timestamp",
         "_last_est_packet",
         "_last_raw_packet",
         "_last_velocity",
@@ -34,7 +34,6 @@ class DataGenerator:
 
     def __init__(self):
         """Initializes the data generator object"""
-        self._current_timestamp: np.float64 = np.float64(0.0)
         self._last_est_packet: EstimatedDataPacket | None = None
         self._last_raw_packet: RawDataPacket | None = None
         self._last_velocity: np.float64 = np.float64(0.0)
@@ -86,169 +85,50 @@ class DataGenerator:
 
         return np.array([motor_timestamps, motor_thrusts])
 
-    def _first_update(self) -> npt.NDArray:
+    def _get_first_packet(
+            self,packet_type: RawDataPacket | EstimatedDataPacket
+            ) -> RawDataPacket | EstimatedDataPacket:
         """
         Sets up the initial values for the estimated and raw data packets. This should
         only be called once, and all values will be approximate launch pad conditions.
 
-        :return: numpy array containing a raw data packet and an estimated data packet
-            respectively, immitating initial conditions on the launch pad.
+        :param packet_type: either RawDataPacket or EstimatedDataPacket class objects.
+            used as identifier.
+
+        :return: data packet of the specified type with launch pad conditions.
         """
         orientation = self._config["rocket_orientation"]
         initial_quaternion, _ = R.align_vectors([0, 0, -1], [orientation])
         initial_quaternion = R.as_quat(initial_quaternion, scalar_first=True)
 
-        return np.array(
-            [
-                RawDataPacket(
-                    0,
-                    scaledAccelX=orientation[0],
-                    scaledAccelY=orientation[1],
-                    scaledAccelZ=orientation[2],
-                    scaledGyroX=0.0,
-                    scaledGyroY=0.0,
-                    scaledGyroZ=0.0,
-                    deltaVelX=0.0,
-                    deltaVelY=0.0,
-                    deltaVelZ=0.0,
-                    deltaThetaX=0.0,
-                    deltaThetaY=0.0,
-                    deltaThetaZ=0.0,
-                ),
-                EstimatedDataPacket(
-                    0,
-                    estOrientQuaternionW=initial_quaternion[0],
-                    estOrientQuaternionX=initial_quaternion[1],
-                    estOrientQuaternionY=initial_quaternion[2],
-                    estOrientQuaternionZ=initial_quaternion[3],
-                    estCompensatedAccelX=GRAVITY * orientation[0],
-                    estCompensatedAccelY=GRAVITY * orientation[1],
-                    estCompensatedAccelZ=GRAVITY * orientation[2],
-                    estPressureAlt=0,
-                    estGravityVectorX=-GRAVITY * orientation[0],
-                    estGravityVectorY=-GRAVITY * orientation[1],
-                    estGravityVectorZ=-GRAVITY * orientation[2],
-                    estAngularRateX=0,
-                    estAngularRateY=0,
-                    estAngularRateZ=0,
-                ),
-            ]
-        )
-
-    def generate_data_packet(self) -> npt.NDArray:
-        """
-        Generates data points and sends to queue
-
-        :return: Numpy list containing a raw data packet, an estimated data packet, or both.
-        """
-        # gets the next timestamp
-        next_timestamp = self.update_timestamp(self._current_timestamp)
-        # If the simulation has just started, we want to just generate the initial data points
-        # and initialize "self._last_" variables
-        if any(packet is None for packet in [self._last_est_packet, self._last_raw_packet]):
-            self._last_raw_packet, self._last_est_packet = self._first_update()
-            self._current_timestamp = next_timestamp
-            return np.array([self._last_raw_packet, self._last_est_packet])
-
-        raw_time_step = self._config["raw_time_step"]
-        est_time_step = self._config["est_time_step"]
-
-        # creates a boolean array to indicate whether the next timestamp will have a raw
-        # data packet, an estimated data packet, or both.
-        packet_type_flag = np.array(
-            [
-                any(np.isclose(next_timestamp % raw_time_step, [0, raw_time_step])),
-                any(np.isclose(next_timestamp % est_time_step, [0, est_time_step])),
-            ]
-        )
-
-        # just as a short-hand
-        orientation = self._config["rocket_orientation"]
-
-        # initializes the net force, drag force and thrust (if applicable) will be added
-        net_force = 0
-        # finds force from thrust curve data, if the timestamp is before the cutoff time
-        # of the motor
-        if next_timestamp <= self._thrust_data[0][-1]:
-            net_force = np.interp(
-                next_timestamp, self._thrust_data[0], self._thrust_data[1]
-            )
-        # subtracts drag force from net force
-        net_force -= self._calculate_drag_force(self._last_velocity)
-        # subtracts weight force from net force
-        net_force -= GRAVITY*self._config["rocket_mass"]
-        vertical_linear_accel = net_force / self._config["rocket_mass"]
-
-        new_packets = np.array([None, None])
-
-        # assembles the raw data packet
-        if packet_type_flag[0]:
-            # scaled acceleration is compensated, but divided by gravity. On launch pad, the
-            # vertical sacled acceleration will be -1.00 (if negative is vertical).
-            # this value should always be positive though (during motor burn)
-            vert_scaled_accel = 1 + vertical_linear_accel / GRAVITY
-
-            # gets the last vertical scaled accel, flips depending on the vertical index
-            last_vert_scaled_accel = [
-                self._last_raw_packet.scaledAccelX,
-                self._last_raw_packet.scaledAccelY,
-                self._last_raw_packet.scaledAccelZ,
-            ][self._vertical_index]
-
-            # finds the vertical delta velocity
-            vert_delta_v = GRAVITY * (vert_scaled_accel - last_vert_scaled_accel) * raw_time_step
-
-            raw_data_packet = RawDataPacket(
-                next_timestamp * 1e9,
-                scaledAccelX=vert_scaled_accel * orientation[0],
-                scaledAccelY=vert_scaled_accel * orientation[1],
-                scaledAccelZ=vert_scaled_accel * orientation[2],
+        packet = None
+        if packet_type == RawDataPacket:
+            packet =  RawDataPacket(
+                0,
+                scaledAccelX=orientation[0],
+                scaledAccelY=orientation[1],
+                scaledAccelZ=orientation[2],
                 scaledGyroX=0.0,
                 scaledGyroY=0.0,
                 scaledGyroZ=0.0,
-                deltaVelX=vert_delta_v * orientation[0],
-                deltaVelY=vert_delta_v * orientation[1],
-                deltaVelZ=vert_delta_v * orientation[2],
+                deltaVelX=0.0,
+                deltaVelY=0.0,
+                deltaVelZ=0.0,
                 deltaThetaX=0.0,
                 deltaThetaY=0.0,
                 deltaThetaZ=0.0,
             )
-            # appends to array
-            new_packets[0] = raw_data_packet
-
-            # updates last packet
-            self._last_raw_packet = raw_data_packet
-
-        # Assembles the estimated data packet
-        if packet_type_flag[1]:
-            # default value is positive during motor burn
-            vert_comp_accel = vertical_linear_accel + GRAVITY
-
-            # gets vertical velocity and finds updated altitude
-            vert_velocity = self._calculate_vertical_velocity(vert_comp_accel, est_time_step)
-            last_altitude = self._last_est_packet.estPressureAlt
-            new_altitude = last_altitude + vert_velocity * est_time_step
-
-            # gets previous quaternion
-            last_quat = np.array(
-                [
-                    self._last_est_packet.estOrientQuaternionW,
-                    self._last_est_packet.estOrientQuaternionX,
-                    self._last_est_packet.estOrientQuaternionY,
-                    self._last_est_packet.estOrientQuaternionZ,
-                ]
-            )
-
-            est_data_packet = EstimatedDataPacket(
-                next_timestamp * 1e9,
-                estOrientQuaternionW=last_quat[0],
-                estOrientQuaternionX=last_quat[1],
-                estOrientQuaternionY=last_quat[2],
-                estOrientQuaternionZ=last_quat[3],
-                estCompensatedAccelX=vert_comp_accel * orientation[0],
-                estCompensatedAccelY=vert_comp_accel * orientation[1],
-                estCompensatedAccelZ=vert_comp_accel * orientation[2],
-                estPressureAlt=new_altitude,
+        else:
+            packet =  EstimatedDataPacket(
+                0,
+                estOrientQuaternionW=initial_quaternion[0],
+                estOrientQuaternionX=initial_quaternion[1],
+                estOrientQuaternionY=initial_quaternion[2],
+                estOrientQuaternionZ=initial_quaternion[3],
+                estCompensatedAccelX=GRAVITY * orientation[0],
+                estCompensatedAccelY=GRAVITY * orientation[1],
+                estCompensatedAccelZ=GRAVITY * orientation[2],
+                estPressureAlt=0,
                 estGravityVectorX=-GRAVITY * orientation[0],
                 estGravityVectorY=-GRAVITY * orientation[1],
                 estGravityVectorZ=-GRAVITY * orientation[2],
@@ -257,16 +137,115 @@ class DataGenerator:
                 estAngularRateZ=0,
             )
 
-            # appends to array
-            new_packets[1] = est_data_packet
+        return packet
 
-            # updates last packet
-            self._last_est_packet = est_data_packet
+    def generate_raw_data_packet(self) -> RawDataPacket:
+        """
+        Generates a raw data packet containing randomly generated data points
 
-        # updates the timestamp
-        self._current_timestamp = next_timestamp
+        :return: RawDataPacket
+        """
+        # creating shorthand variables from config
+        time_step = self._config["raw_time_step"]
+        orientation = self._config["rocket_orientation"]
 
-        return new_packets
+        # If the simulation has just started, we want to just generate the initial raw packet
+        # and initialize "self._last_" variables
+        if self._last_raw_packet is None:
+            self._last_raw_packet = self._get_first_packet(RawDataPacket)
+            return self._last_raw_packet
+
+        # calculates the timestamp for this packet (in seconds)
+        next_timestamp = (self._last_raw_packet.timestamp/1e9) + time_step
+
+        # calculates the net force and vertical scaled acceleration
+        net_force = self._calculate_net_force(next_timestamp,self._last_velocity)
+        vertical_scaled_accel = net_force / (self._config["rocket_mass"] * GRAVITY)
+
+        # calculates vertical delta velocity
+        last_vertical_scaled_accel = self._get_vertical_data_point("scaledAccel")
+        vert_delta_v = (vertical_scaled_accel - last_vertical_scaled_accel) * GRAVITY * time_step
+
+        packet =  RawDataPacket(
+            next_timestamp * 1e9,
+            scaledAccelX=vertical_scaled_accel * orientation[0],
+            scaledAccelY=vertical_scaled_accel * orientation[1],
+            scaledAccelZ=vertical_scaled_accel * orientation[2],
+            scaledGyroX=0.0,
+            scaledGyroY=0.0,
+            scaledGyroZ=0.0,
+            deltaVelX=vert_delta_v * orientation[0],
+            deltaVelY=vert_delta_v * orientation[1],
+            deltaVelZ=vert_delta_v * orientation[2],
+            deltaThetaX=0.0,
+            deltaThetaY=0.0,
+            deltaThetaZ=0.0,
+        )
+
+        # updates last raw data packet
+        self._last_raw_packet = packet
+
+        return packet
+
+    def generate_estimated_data_packet(self) -> EstimatedDataPacket:
+        """
+        Generates an estimated data packet containing randomly generated data points
+
+        :return: EstimatedDataPacket
+        """
+        # creating shorthand variables from config
+        time_step = self._config["est_time_step"]
+        orientation = self._config["rocket_orientation"]
+
+        # If the simulation has just started, we want to just generate the initial estimated packet
+        # and initialize "self._last_" variables
+        if self._last_est_packet is None:
+            self._last_est_packet = self._get_first_packet(EstimatedDataPacket)
+            return self._last_raw_packet
+
+        # calculates the timestamp for this packet (in seconds)
+        next_timestamp = (self._last_est_packet.timestamp/1e9) + time_step
+
+        # calculates the net force and vertical accelerations
+        net_force = self._calculate_net_force(next_timestamp,self._last_velocity)
+        vertical_linear_accel = net_force / self._config["rocket_mass"]
+        vertical_comp_accel = vertical_linear_accel + GRAVITY
+
+        # gets vertical velocity and finds updated altitude
+        vert_velocity = self._calculate_vertical_velocity(vertical_comp_accel, time_step)
+        new_altitude = self._last_est_packet.estPressureAlt + vert_velocity * time_step
+
+        # gets previous quaternion
+        last_quat = np.array(
+            [
+                self._last_est_packet.estOrientQuaternionW,
+                self._last_est_packet.estOrientQuaternionX,
+                self._last_est_packet.estOrientQuaternionY,
+                self._last_est_packet.estOrientQuaternionZ,
+            ])
+
+        packet = EstimatedDataPacket(
+            next_timestamp * 1e9,
+            estOrientQuaternionW=last_quat[0],
+            estOrientQuaternionX=last_quat[1],
+            estOrientQuaternionY=last_quat[2],
+            estOrientQuaternionZ=last_quat[3],
+            estCompensatedAccelX=vertical_comp_accel * orientation[0],
+            estCompensatedAccelY=vertical_comp_accel * orientation[1],
+            estCompensatedAccelZ=vertical_comp_accel * orientation[2],
+            estPressureAlt=new_altitude,
+            estGravityVectorX=-GRAVITY * orientation[0],
+            estGravityVectorY=-GRAVITY * orientation[1],
+            estGravityVectorZ=-GRAVITY * orientation[2],
+            estAngularRateX=0,
+            estAngularRateY=0,
+            estAngularRateZ=0,
+        )
+
+        # updates last estimated packet
+        self._last_est_packet = packet
+
+        return packet
 
     def _calculate_vertical_velocity(self, acceleration, time_diff) -> np.float64:
         """
@@ -286,47 +265,53 @@ class DataGenerator:
 
         return vertical_velocity
 
-    def update_timestamp(self, current_timestamp: np.float64) -> np.float64:
+    def _calculate_net_force(self, timestamp, velocity) -> np.float64:
         """
-        Updates the current timestamp of the data generator, based off time step defined in config.
-        Will also determine if the next timestamp will be a raw packet, estimated packet, or both.
+        Calculates the drag force, thrust force, and weight, and sums them.
 
-        :param current_timestamp: the current timestamp of the simulation
+        :param timestamp: the timestamp of the rocket to calcuate the net forces at
+        :param velocity: the vertical velocity at the given instant
 
-        :return: the updated current timestamp, rounded to 3 decimals
+        :return: float containing the net force. Thrust is positive, drag and weight is negative.
         """
-
-        # finding whether the raw or estimated data packets have a lower time_step
-        lowest_dt = min(self._config["raw_time_step"], self._config["est_time_step"])
-        highest_dt = max(self._config["raw_time_step"], self._config["est_time_step"])
-
-        # checks if current time is a multiple of the highest and/or lowest time step
-        at_low = any(np.isclose(current_timestamp % lowest_dt, [0, lowest_dt]))
-        at_high = any(np.isclose(current_timestamp % highest_dt, [0, highest_dt]))
-
-        # If current timestamp is a multiple of both, the next timestamp will be the
-        # the current timestamp + the lower time steps
-        if all([at_low, at_high]):
-            return np.round(current_timestamp + lowest_dt, 3)
-
-        # If timestamp is a multiple of just the lowest time step, the next will be
-        # either current + lowest, or the next timestamp that is divisible by the highest
-        if at_low and not at_high:
-            dt = min(lowest_dt, highest_dt - (current_timestamp % highest_dt))
-            return np.round(current_timestamp + dt, 3)
-
-        # If timestamp is a multiple of only the highest time step, the next will
-        # always be the next timestamp that is divisible by the lowest
-        if at_high and not at_low:
-            return np.round(current_timestamp + lowest_dt - (current_timestamp % lowest_dt), 3)
-
-        # This would happen if the input current timestamp is not a multiple of the raw
-        # or estimated time steps, or if there is a rounding/floating point error.
-        raise ValueError("Could not update timestamp, time stamp is invalid")
-
-    def _calculate_drag_force(self,velocity):
-        # we could probably actually calculate this, maybe we can set temp as a constant?
-        roe = 1.1
+        # we could probably actually calculate air density, maybe we set temperature as constant?
+        air_density = 1.1
         reference_area = self._config["reference_area"]
         drag_coefficient = self._config["drag_coefficient"]
-        return 0.5 * roe * reference_area * drag_coefficient * velocity**2
+        rocket_mass = self._config["rocket_mass"]
+
+        drag_force =  0.5 * air_density * reference_area * drag_coefficient * velocity**2
+        weight_force = GRAVITY * rocket_mass
+        thrust_force = 0.0
+
+        # thrust force is non-zero if the timestamp is within the timeframe of
+        # the motor burn
+        if timestamp <= self._thrust_data[0][-1]:
+            thrust_force = np.interp(
+                timestamp, self._thrust_data[0], self._thrust_data[1]
+            )
+        return thrust_force - weight_force - drag_force
+
+    def _get_vertical_data_point(self,string_identifier: str) -> np.float64:
+        """
+        gets the last vertical data point specified from a vector data attribute by using IMU
+        orientation in config
+
+        :param string_identfier: a string representing the exact attribute name of the data packet
+
+        :return: float containing the specified data point
+        """
+        # Dynamically retrieve x, y, z components based on the identifier
+        values = [
+            getattr(self._last_raw_packet, f"{string_identifier}X", None),
+            getattr(self._last_raw_packet, f"{string_identifier}Y", None),
+            getattr(self._last_raw_packet, f"{string_identifier}Z", None),
+        ]
+
+        # Just an edge case, but I really hope this never happens
+        if any(value is None for value in values):
+            raise AttributeError(
+                f"Could not find all components for identifier '{string_identifier}'."
+                )
+
+        return values[self._vertical_index]
--- simulator/sim_imu.py
+++ simulator/sim_imu.py
@@ -3,7 +3,11 @@
 import contextlib
 import multiprocessing
 import time
+from pathlib import Path
 from typing import TYPE_CHECKING
+
+import numpy as np
+import yaml
 
 if TYPE_CHECKING:
     from airbrakes.data_handling.imu_data_packet import IMUDataPacket
@@ -20,10 +24,12 @@ class SimIMU(IMU):
     """
 
     __slots__ = (
+        "_config",
         "_data_fetch_process",
         "_data_generator",
         "_data_queue",
         "_running",
+        "_timestamp",
     )
 
     def __init__(self):
@@ -31,6 +37,12 @@ class SimIMU(IMU):
         Initializes the object that pretends to be an IMU for testing purposes by returning
         randomly generated data.
         """
+        self._timestamp: np.float64 = np.float64(0.0)
+        # loads the sim_config.yaml file
+        config_path = Path("simulator/sim_config.yaml")
+        with config_path.open(mode="r", newline="") as file:
+            self._config: dict = yaml.safe_load(file)
+
         # This limits the queue size to a very high limit, because the data generator will
         # generate all of the data before the imu reads it
         self._data_queue: multiprocessing.Queue[IMUDataPacket] = multiprocessing.Queue(
@@ -48,21 +60,69 @@ class SimIMU(IMU):
 
         self._data_generator = DataGenerator()
 
+    def update_timestamp(self, current_timestamp: np.float64) -> np.float64:
+        """
+        Updates the current timestamp of the data generator, based off time step defined in config.
+        Will also determine if the next timestamp will be a raw packet, estimated packet, or both.
+
+        :param current_timestamp: the current timestamp of the simulation
+
+        :return: the updated current timestamp, rounded to 3 decimals
+        """
+
+        # finding whether the raw or estimated data packets have a lower time_step
+        lowest_dt = min(self._config["raw_time_step"], self._config["est_time_step"])
+        highest_dt = max(self._config["raw_time_step"], self._config["est_time_step"])
+
+        # checks if current time is a multiple of the highest and/or lowest time step
+        at_low = any(np.isclose(current_timestamp % lowest_dt, [0, lowest_dt]))
+        at_high = any(np.isclose(current_timestamp % highest_dt, [0, highest_dt]))
+
+        # If current timestamp is a multiple of both, the next timestamp will be the
+        # the current timestamp + the lower time steps
+        if all([at_low, at_high]):
+            return np.round(current_timestamp + lowest_dt, 3)
+
+        # If timestamp is a multiple of just the lowest time step, the next will be
+        # either current + lowest, or the next timestamp that is divisible by the highest
+        if at_low and not at_high:
+            dt = min(lowest_dt, highest_dt - (current_timestamp % highest_dt))
+            return np.round(current_timestamp + dt, 3)
+
+        # If timestamp is a multiple of only the highest time step, the next will
+        # always be the next timestamp that is divisible by the lowest
+        if at_high and not at_low:
+            return np.round(current_timestamp + lowest_dt - (current_timestamp % lowest_dt), 3)
+
+        # This would happen if the input current timestamp is not a multiple of the raw
+        # or estimated time steps, or if there is a rounding/floating point error.
+        raise ValueError("Could not update timestamp, time stamp is invalid")
+
     def _fetch_data_loop(self) -> None:
         """A wrapper function to suppress KeyboardInterrupt exceptions when obtaining generated
         data."""
+
+        raw_dt = self._config["raw_time_step"]
+        est_dt = self._config["est_time_step"]
+
         # unfortunately, doing the signal handling isn't always reliable, so we need to wrap the
         # function in a context manager to suppress the KeyboardInterrupt
         with contextlib.suppress(KeyboardInterrupt):
-            for _ in range(21000):
-                packets = self._data_generator.generate_data_packet()
-                timestamp = 0
-                if packets[0] is not None:
-                    self._data_queue.put(packets[0])
-                    timestamp = packets[0].timestamp / 1e9
-                if packets[1] is not None:
-                    self._data_queue.put(packets[1])
-                    timestamp = packets[1].timestamp / 1e9
+            while self._data_generator._last_velocity > -1:
+                # starts timer
+                start_time = time.time()
 
-                dt = self._data_generator.update_timestamp(timestamp) - timestamp
-                time.sleep(dt)
+                # if the timestamp is a multiple of the raw time step, generate a raw data packet.
+                if any(np.isclose(self._timestamp % raw_dt, [0, raw_dt])):
+                    self._data_queue.put(self._data_generator.generate_raw_data_packet())
+
+                # if the timestamp is a multiple of the est time step, generate an est data packet.
+                if any(np.isclose(self._timestamp % est_dt, [0, est_dt])):
+                    self._data_queue.put(self._data_generator.generate_estimated_data_packet())
+
+                # updates the timestamp and sleeps until next packet is ready in real-time
+                time_step = self.update_timestamp(self._timestamp) - self._timestamp
+                self._timestamp += time_step
+                end_time = time.time()
+                time.sleep(max(0.0, time_step - (end_time - start_time)))
+
