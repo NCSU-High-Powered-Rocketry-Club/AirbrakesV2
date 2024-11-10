1bca6dbd27a721f89e7a674d875c4fa095a66301
jgelia <jgelia@ncsu.edu>
2024-11-09 22:32:39 -0500

    Cleaned up typing for SimulationConfig

--- airbrakes/simulation/sim_config.py
+++ airbrakes/simulation/sim_config.py
@@ -11,8 +11,8 @@ class SimulationConfig:
                  rocket_mass: np.float64,
                  reference_area: np.float64,
                  rocket_orientation: npt.NDArray[np.float64],
-                 launch_rod_angle: np.float64,
-                 launch_rod_direction: np.float64):
+                 launch_rod_angle: npt.NDArray[np.float64],
+                 launch_rod_direction: npt.NDArray[np.float64]):
         # Time steps for data packet generation in the simulation
         self.raw_time_step = np.float64(raw_time_step)
         self.est_time_step = np.float64(est_time_step)
@@ -34,28 +34,28 @@ class SimulationConfig:
 
 
 FULL_SCALE_CONFIG = SimulationConfig(
-    raw_time_step=0.001,
-    est_time_step=0.002,
+    raw_time_step=np.float64(0.001),
+    est_time_step=np.float64(0.002),
     motor="AeroTech_L1940X",
-    drag_coefficient=0.4,
-    rocket_mass=14.5,
-    reference_area=0.01929,
-    rocket_orientation=[0, 0, -1],
-    launch_rod_angle=[10],
-    launch_rod_direction=[0]
+    drag_coefficient=np.float64(0.4),
+    rocket_mass=np.float64(14.5),
+    reference_area=np.float64(0.01929),
+    rocket_orientation=np.array([0, 0, -1]),
+    launch_rod_angle=np.array([10]),
+    launch_rod_direction=np.array([0])
 )
 
 # TODO: get actual values for sub scale
 SUB_SCALE_CONFIG = SimulationConfig(
-    raw_time_step=0.001,
-    est_time_step=0.002,
+    raw_time_step=np.float64(0.001),
+    est_time_step=np.float64(0.002),
     motor="PLACEHOLDER",
-    drag_coefficient=0.4,
-    rocket_mass=0,
-    reference_area=0.001929,
-    rocket_orientation=[0, 0, -1],
-    launch_rod_angle=[10],
-    launch_rod_direction=[0]
+    drag_coefficient=np.float64(0.4),
+    rocket_mass=np.float64(0),
+    reference_area=np.float64(0.001929),
+    rocket_orientation=np.array([0, 0, -1]),
+    launch_rod_angle=np.array([10]),
+    launch_rod_direction=np.array([0])
 )
 
 
