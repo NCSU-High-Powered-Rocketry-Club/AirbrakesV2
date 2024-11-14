6e6c0799e8ab15b85d391ea0cb365269f44f08a1
DirtyPi09 <pidirty09@gmail.com>
2024-11-14 12:56:25 -0500

    Revert "Got rid of nobuzz code"
    This reverts commit f7b10e24418bd680ef3a692598c70e5a39abdb39.

--- airbrakes/hardware/servo.py
+++ airbrakes/hardware/servo.py
@@ -1,9 +1,12 @@
 """Module which contains the Servo class, representing a servo motor that controls the extension of
 the airbrakes."""
 
+import threading
+import time
+
 import gpiozero
 
-from constants import ServoExtension
+from constants import SERVO_DELAY, ServoExtension
 
 
 class Servo:
@@ -40,13 +43,37 @@ class Servo:
         """
         Extends the servo to the maximum extension.
         """
-        self._set_extension(ServoExtension.MAX_EXTENSION)
+        # We have to use threading to avoid blocking the main thread because our extension methods
+        # sleep
+        thread = threading.Thread(target=self._extend_then_no_buzz)
+        thread.start()
 
     def set_retracted(self) -> None:
         """
         Retracts the servo to the minimum extension.
         """
+        thread = threading.Thread(target=self._retract_then_no_buzz)
+        thread.start()
+
+    def _extend_then_no_buzz(self) -> None:
+        """
+        Extends the servo then stops buzzing. This extends the servo to the maximum extension,
+        waits for the servo to reach the physical end of the air brakes, and then sets its
+        extension to its actual extension.
+        """
+        self._set_extension(ServoExtension.MAX_EXTENSION)
+        time.sleep(SERVO_DELAY)
+        self._set_extension(ServoExtension.MAX_NO_BUZZ)
+
+    def _retract_then_no_buzz(self) -> None:
+        """
+        Retracts the servo then stops buzzing. This retracts the servo to the minimum extension,
+        waits for the servo to reach the physical end of the air brakes, and then sets its
+        extension to its actual extension.
+        """
         self._set_extension(ServoExtension.MIN_EXTENSION)
+        time.sleep(SERVO_DELAY)
+        self._set_extension(ServoExtension.MIN_NO_BUZZ)
 
     def _set_extension(self, extension: ServoExtension) -> None:
         """
