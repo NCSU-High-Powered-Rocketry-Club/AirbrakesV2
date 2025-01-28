b8da9f14ee71f6cdad18722c0727db9c9ffeb067
Harshil <37377066+harshil21@users.noreply.github.com>
2025-01-28 16:02:38 -0500

    change import style in IMU

--- airbrakes/hardware/imu.py
+++ airbrakes/hardware/imu.py
@@ -4,16 +4,13 @@ import contextlib
 import multiprocessing
 import sys
 
-# Try to import the MSCL library, if it fails, warn the user. mscl does not work on Windows with
-# Python 3.13.
-with contextlib.suppress(ImportError):
-    from python_mscl import mscl
-
 # If we are not on windows, we can use the faster_fifo library to speed up the queue operations
 if sys.platform != "win32":
     from faster_fifo import Queue
-else:
-    pass
+
+    # Try to import the MSCL library, if it fails, warn the user. mscl does not work on Windows with
+    # Python 3.13.
+    from python_mscl import mscl
 
 from airbrakes.constants import (
     BUFFER_SIZE_IN_BYTES,
