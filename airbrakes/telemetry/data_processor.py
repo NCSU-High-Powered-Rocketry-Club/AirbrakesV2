166b90d596f9103e0560529321a7e6f1d03f91ad
Jack <jackgeorgeelia@gmail.com>
2025-03-05 00:56:19 -0500

    Fleshed out how I'll implement this

--- airbrakes/telemetry/data_processor.py
+++ airbrakes/telemetry/data_processor.py
@@ -25,11 +25,14 @@ class DataProcessor:
         "_current_orientation_quaternions",
         "_data_packets",
         "_initial_altitude",
+        "_integrating_for_altitude",
         "_last_data_packet",
         "_max_altitude",
         "_max_vertical_velocity",
+        "_previous_altitude_data_points",
         "_previous_vertical_velocity",
         "_rotated_accelerations",
+        "_store_altitude_data",
         "_time_differences",
         "_vertical_velocities",
     )
@@ -52,6 +55,10 @@ class DataProcessor:
         self._rotated_accelerations: npt.NDArray[np.float64] = np.array([0.0])
         self._data_packets: list[EstimatedDataPacket] = []
         self._time_differences: npt.NDArray[np.float64] = np.array([0.0])
+        self._store_altitude_data = False
+        self._integrating_for_altitude = False
+        # This a list of tuples with (timestamp in seconds, altitude)
+        self._previous_altitude_data_points: list[tuple[float, float]] = []
 
     def __str__(self) -> str:
         return (
@@ -152,6 +159,12 @@ class DataProcessor:
         self._current_altitudes = self._calculate_current_altitudes()
         self._max_altitude = max(self._current_altitudes.max(), self._max_altitude)
 
+        if self._store_altitude_data:
+            # Stores the altitude data points for the quadratic fit
+            self._previous_altitude_data_points.append(
+                (convert_ns_to_s(self.current_timestamp), self.current_altitude)
+            )
+
         # Store the last data point for the next update
         self._last_data_packet = data_packets[-1]
 
@@ -171,6 +184,36 @@ class DataProcessor:
             )
             for i in range(len(self._data_packets))
         ]
+
+    def start_storing_altitude_data(self) -> None:
+        """
+        Starts storing altitude data for the purpose of calculating the average altitude during
+        the coast phase.
+        """
+        self._store_altitude_data = True
+        self._previous_altitude_data_points = ([], [])
+
+    def prepare_for_extending_airbrakes(self) -> None:
+        """
+        When we extend the airbrakes, it messes with the pressure sensor which messes up the
+        altitude data. Additionally, the velocity data could have accumulated error due to the
+        strong acceleration from the motor burn. Because of these things, this function makes the
+        data processor start integrating for altitude and "calibrates" the velocity.
+        """
+        self._store_altitude_data = False
+        self._integrating_for_altitude = True
+
+        if len(self._previous_altitude_data_points[0]) > 0:
+            # First we have to only keep the data points with unique altitudes
+            unique_time_stamps = [self._previous_altitude_data_points[0][0]]
+            unique_altitudes = [self._previous_altitude_data_points[1][0]]
+            for time_stamp, altitude in self._previous_altitude_data_points:
+                if altitude != unique_altitudes[-1]:
+                    unique_time_stamps.append(time_stamp)
+                    unique_altitudes.append(altitude)
+
+            coeffs = np.polyfit(unique_time_stamps, unique_altitudes, 2)
+            np.poly1d(coeffs)
 
     def _first_update(self) -> None:
         """
